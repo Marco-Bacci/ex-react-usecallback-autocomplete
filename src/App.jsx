e2c57ad826f49826fd612ffe8e246c82f1f856a3
Marco Bacci <marcobacci94@gmail.com>
2025-12-02 15:41:44 +0100

    Feat: milestone 1completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,35 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 function App() {
-  const [query, setQuery] = useState("");
+  const [search, setSearch] = useState("");
+  const [results, setResults] = useState([]);
+  console.log(results);
+
+  useEffect(() => {
+    if (search.trim() === "") {
+      setResults([]);
+      return;
+    }
+    fetch(`http://localhost:3333/products?search=${search}`)
+      .then((res) => res.json())
+      .then((data) => setResults(data))
+      .catch((error) => console.error(error));
+  }, [search]);
 
   return (
     <>
       <input
         type="text"
         placeholder="cerca prodotto..."
-        value={query}
-        onChange={(e) => setQuery(e.target.value)}
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
       />
+      <div className="suggerimenti">
+        {results.map((product) => {
+          return <p key={product.id}>{product.name}</p>;
+        })}
+      </div>
+      
     </>
   );
 }
